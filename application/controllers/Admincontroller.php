d4e1078ff40048518d6757db2db85b02d343c4e6
zidyazid <darkyazid132@gmail.com>
2021-09-07 14:20:21 +0700

    melakukan perubahan pada admincontroller

--- application/controllers/Admincontroller.php
+++ application/controllers/Admincontroller.php
@@ -3,15 +3,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Admincontroller extends CI_Controller
 {
-	public function __construct()
-	{
-		parent::__construct();
-		is_logged_in();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
 
-	public function index(){
-        $data['title'] = "Dashboard Page";
-			$data['judul'] = "Admin Page";
+    public function index(){
+        $data['title'] = "Dashboard";
+            $data['judul'] = "Admin Page";
             $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
             // $data['menu'] = $this->db->get_where('user_sub_menu', ['role_id' => $this->session->userdata('role_id')])->row_array();
             $this->load->view('komponen/header', $data);
@@ -19,5 +19,44 @@ class Admincontroller extends CI_Controller
             // $this->load->view('komponen/topnav', $data);
             $this->load->view('admin/index', $data);
             // $this->load->view('komponen/footer', $data);
-        }
+    }
+
+    public function pertambahan_penduduk()
+    {
+        $data['title'] = "Pertambahan Penduduk";
+        $data['judul'] = "Pertambahan Penduduk";
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        // $data['menu'] = $this->db->get_where('user_sub_menu', ['role_id' => $this->session->userdata('role_id')])->row_array();
+        $this->load->view('komponen/header', $data);
+        $this->load->view('komponen/sidebar', $data);
+        // $this->load->view('komponen/topnav', $data);
+        $this->load->view('admin/pertambahan_penduduk', $data);
+        // $this->load->view('komponen/footer', $data);
+    }
+
+    public function pengurangan_penduduk()
+    {
+        $data['title'] = "Pengurangan Penduduk";
+        $data['judul'] = "Pengurangan Penduduk";
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        // $data['menu'] = $this->db->get_where('user_sub_menu', ['role_id' => $this->session->userdata('role_id')])->row_array();
+        $this->load->view('komponen/header', $data);
+        $this->load->view('komponen/sidebar', $data);
+        // $this->load->view('komponen/topnav', $data);
+        $this->load->view('admin/pengurangan_penduduk', $data);
+        // $this->load->view('komponen/footer', $data);
+    }
+
+    public function total_penduduk()
+    {
+        $data['title'] = "Total Penduduk";
+        $data['judul'] = "Total Penduduk";
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        // $data['menu'] = $this->db->get_where('user_sub_menu', ['role_id' => $this->session->userdata('role_id')])->row_array();
+        $this->load->view('komponen/header', $data);
+        $this->load->view('komponen/sidebar', $data);
+        // $this->load->view('komponen/topnav', $data);
+        $this->load->view('admin/total_penduduk', $data);
+        // $this->load->view('komponen/footer', $data);
+    }
 }
